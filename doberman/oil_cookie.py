73a0965199ab3a6dfa37841c85f3ed2aae0f9836
Ryan Harper <ryan.harper@canonical.com>
2014-09-09 13:43:47 -0500

    initial import

---
+++ doberman/oil_cookie.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python2
+
+import sys
+from doberman.common import pycookiecheat, utils
+
+LOG = utils.get_logger('doberman.oilcookie')
+
+def main():
+    utils.write_cookie_file()
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main()) 
