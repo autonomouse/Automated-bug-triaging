0ba445d1822173bd3ef66625609de4084779ac74
Larry Michel larry.michel@canonical.com <>
2014-09-09 17:01:25 -0500

    Minor changes to fix a couple of issues after merging. Renaming of variables and comment modifications for consistency with launchpad's searchTask()

--- doberman/getbugs.py
+++ doberman/getbugs.py
@@ -8,8 +8,10 @@
 #    a) Get the bug first.. if bug number was not set during instantiation,
 #       then you need to set it before calling this rooting.
 #    b) Get the tags or get the duplicates using the method provided.
+# Note that lauchpad's searchTask() method is being used to get a list of
+# tasks for the bugs in a distribution.
 #
-#
+# EXAMPLE CODE for using this module.
 ##launchpad = Launchpad.login_with('getbugs.py', 'production')
 ##
 ##people = launchpad.people
@@ -33,11 +35,11 @@
 ##        print("Duplicate bug: " + duplicates[i])
 
 ### EXAMPLES for getting bugs by tags
-##bug = bugObj.getFirstTaggedBug()
+##bug = bugObj.getFirstTaggedBugTask()
 ##if (bug != None):
 ##    print(bug.bug.tags)
 ##
-##bug = bugObj.getNextTaggedBug()
+##bug = bugObj.getNextTaggedBugTask()
 ##if (bug != None):
 ##    print(bug.bug.tags)
 
@@ -56,21 +58,23 @@ class LaunchpadBugs():
         self.launchpadObj = inLaunchpadObj
         self.currentIdx = 0
         self.bugtasks = []
+        self.bugtaskslength = 0
         self.currentBug = None
         self.currentBugDuplicates = []
+        self.getAllTasks()
 
-    # Method: getBugByNumber()
-    # Description: Return current bug
+    # Method: setBugByNumber()
+    # Description: Set current bug number
     def setBugNumber(self, numinput):
         self.bugnumber = str(numinput)
 
     # Method: getBugNumber()
-    # Description: Return current bug
+    # Description: Return current bug number to caller
     def getBugNumber(self):
         return self.bugnumber
 
     # Method: getBugByBugNumber()
-    # Description: Return current bug
+    # Description: Return bug object to caller and update current bug task.
     def getBugByBugNumber(self):
         if self.getBugNumber() is None:
             msg = "ERROR - bug number is not set - call setNumber() first"
@@ -90,7 +94,7 @@ class LaunchpadBugs():
         return self.currentBug
 
     # Method: getTagsByBugNumber()
-    # Description: Return current bug
+    # Description: Return tag object for current bug.
     def getTagsByBugNumber(self):
         if self.currentBug is None:
             msg = "ERROR - There is no bug in memory" + \
@@ -101,7 +105,7 @@ class LaunchpadBugs():
         return self.currentBug.bug.tags
 
     # Method: getCurrentBugDuplicates()
-    # Description: Return current bug
+    # Description: Return current bug duplicate list for current bug.
     def getCurrentBugDuplicates(self):
         retdup = []
         if (self.currentBug is None):
@@ -116,72 +120,76 @@ class LaunchpadBugs():
         return retdup
 
     # Method: getAllTasks()
-    # Description: This returns all the bugs in distribution
-    #
+    # Description: This returns all the tasks in distribution.
     def getAllTasks(self):
         distobj = self.launchpadObj.distributions[self.dist]
         self.bugtasks = distobj.searchTasks()
+        self.bugtaskslength = len(self.bugtasks)
 
-    # Method: getFirstBug()
-    # Description: Return the first bug in the list.
-    def getFirstBug(self):
-        bug = self.bugtasks[0]
+    # Method: getFirstTask()
+    # Description: Return the first bug in the list of tasks.
+    def getFirstTask(self):
+        bugtask = self.bugtasks[0]
         self.currentIdx = 1
-        return bug
+        return bugtask
 
-    # Method: getNextBug()
-    # Description: Return the next bug in the list.
-    def getNextBug(self):
-        if (self.currentIdx == self.len):
-            bug = None
+    # Method: getNextTask()
+    # Description: Return the next bug task in the list.
+    def getNextTask(self):
+        if (self.currentIdx == self.bugtaskslength):
+            bugtask = None
         else:
-            bug = self.bugtasks[self.currentIdx]
+            bugtask = self.bugtasks[self.currentIdx]
             self.currentIdx += 1
-        return bug
+        return bugtask
 
-    # Method: getLastBug()
+    # Method: getLastBugTask()
     # Description: Return the first bug in the list.
-    def getLastBug(self):
-        bug = self.bugtasks[self.len - 1]
-        self.currentIdx = self.len
-        return bug
+    def getLastBugTask(self):
+        bug = self.bugtasks[self.bugtaskslength - 1]
+        self.currentIdx = self.bugtaskslength
+        return bugtask
 
     # Method: setTag()
-    # Description: Takes as input space separated list of tags
+    # Description: Takes as input space separated list of tags and update
+    #              internal task list for this class object.
     def setTag(self, intag):
         self.tags = intag.split()
 
     # Method: clearTag()
-    # Description: setTag
+    # Description: clear tags list for this class object.
     def clearTag(self):
         self.tags = []
 
-    # Method: getFirstTaggedBug()
-    # Description: Return the first bug in the list.
-    def getFirstTaggedBug(self):
+    # Method: getFirstTaggedBugTask()
+    # Description: Return the first bug task in the task list returned by
+    #              searchTask().
+    def getFirstTaggedBugTask(self):
         if (self.tags == []):
             msg = "ERROR - There are no tags set - call setTag() first"
             LOG.exception(msg)
             raise ValueError(msg)
         else:
-            return self.getNextTaggedBug()
+            return self.getNextTaggedBugTask()
 
-    # Method: getNextTaggedBug()
-    # Description: Return the next bug in the list.
-    def getNextTaggedBug(self):
+    # Method: getNextTaggedBugTask()
+    # Description: Return the next bug task in the task list returned by
+    #              searchTask().
+    def getNextTaggedBugTask(self):
         ret = None
 
-        if (self.currentIdx == self.len):
+        if (self.currentIdx == self.bugtaskslength):
             # reached end of list
             ret = None
         else:
             numtags = len(self.tags)
             found = 0
 
-            # Look for the next entry which matches all the tags passed in
-            while (found != numtags) and (self.currentIdx != self.len):
-                bug = self.bugtasks[self.currentIdx]
-                bugtags = bug.bug.tags
+            # Look for the next entry in task list which matches all the
+            # tags passed in
+            while (found != numtags) and (self.currentIdx != self.bugtaskslength):
+                bugtask = self.bugtasks[self.currentIdx]
+                bugtags = bugtask.bug.tags
                 found = 0
 
                 # Look through all the entries, looking for a match.
@@ -192,18 +200,18 @@ class LaunchpadBugs():
                             found += 1
                             break
 
-                # point to next bug
+                # point to next bugtask
                 self.currentIdx += 1
 
             # return match or nothing
             if (found == numtags):
-                ret = bug
+                ret = bugtask
 
         return ret
 
-    # Method: getLastTaggedBug()
+    # Method: getLastTaggedBugTask()
     # Description: Return the first bug in the list.
-    def getLastTaggedBug(self):
+    def getLastTaggedBugTask(self):
         # get the next tag until there are no more tags
         # To be completed
         raise ValueError("Not Implemented")
