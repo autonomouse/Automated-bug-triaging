e05e3cb8ae1180ca77f5d8c4aa64b3e52ed2f1f9
Darren Hoyland <darren.hoyland@canonical.com>
2014-12-15 15:43:18 +0000

    Now blanks out the pipeline id

--- doberman/refinery/refinery.py
+++ doberman/refinery/refinery.py
@@ -396,7 +396,7 @@ class Refinery(CrudeAnalysis):
         # replace pipeline id(s) with placeholder:
         for pl in pipelines:
             info.replace(pl, 'AAAAAAAA-BBBB-CCCC-DDDD-EEEEEEEEEEEE')
-        
+
         # replace numbers with 'X'
         info = re.sub(r'\d', 'X', info) if info else ''
 
@@ -417,7 +417,7 @@ class Refinery(CrudeAnalysis):
 
             # Search for failure:
             fails = sorted(re.findall('fail.*', info, re.IGNORECASE))
-        
+
         bug_feedback = " ".join([str(n) for n in (traceback, errs, fails)])
         if (bug_feedback == ' [] []') or not bug_feedback.strip(' '):
             return
