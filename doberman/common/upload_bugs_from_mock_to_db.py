f4d0dc9393800c8dd87f3fc273b0337abc194051
Greg Lutostanski <gregory.lutostanski@canonical.com>
2015-03-10 08:27:02 -0500

    preliminary pass on fixing up bug_uploader

--- doberman/common/upload_bugs_from_mock_to_db.py
+++ doberman/common/upload_bugs_from_mock_to_db.py
@@ -1,54 +1,64 @@
+#!/usr/bin/python
 
 from test_catalog.client.api import TCClient as tc_client
 import yaml
 import json
 
-db = yaml.load(open('/usr/share/doberman/samples/mock_database.yml'))
+DEFAULT_DB_LOCATION = '/usr/share/doberman/samples/mock_database.yml'
+DEFAULT_COOKIE_LOCATION = '/etc/oil-ci/test-catalog-auth.json'
+DEFAULT_ENDPOINT = 'https://oil.canonical.com/api/'
 
-cook = json.load(open('test-catalog-auth.json'))
 
-client = tc_client(endpoint='https://oil.canonical.com/api/', cookies=cook)
+def update_bugs_database(db_location, cookie_location, endpoint):
+    local_db = yaml.load(open(db_location))
+    cookie = json.load(open(cookie_location))
+    client = tc_client(endpoint=endpoint, cookies=cookie)
 
-#db = client.get_bug_info()
-#thing = \
-#"""
-#  "1386679":
-#    category: None
-#    description: "Prodstack: storage backend relation joined"
-#    pipeline_deploy:
-#      -
-#        "console.txt":
-#            regexp:
-#            - "unit. cinder-vnx... machine.  agent-state. error details. hook failed...storage-backend-relation-joined"
-#      -
-#        "juju_status.yaml":
-#            regexp:
-#            - "hook failed...storage.backend.relation.joined.+for cinder-vnx.storage.backend"
-#            - "hook failed...storage.backend.relation.joined.+for cinder-vnx.storage.backend2"
-#        "console.txt":
-#            regexp:
-#            - "udetails. hook failed...storage-backend-relation-joined"
-#"""
-#db = {'bugs': yaml.load(thing)}
+    remote_db = {'bugs': []}
+    try:
+        remote_db = client.get_bug_info(force_refresh=True)
+    except:
+        pass
 
-for bugno, data in db['bugs'].items():
-    del data['description']
-    del data['category']
-    for names, file_regex in data.items():
-        for count, entry in enumerate(file_regex):
-            first = True
-            for file, regexes in entry.items():
-                boolean = 'and'
-                if count > 0 and first:
-                    boolean = 'or'
-                if isinstance(regexes['regexp'], str):
-                    regexes['regexp'] = [regexes['regexp']]
-                for reg in regexes['regexp']:
-                    if reg:
-                        print(bugno, names, file, reg, boolean)
-                        client.add_bug_info(bugno, names, file, reg, boolean)
-                        first = False
-                        boolean = 'and'
+    for bugno, data in local_db['bugs'].items():
+        if 'description' in data:
+            del data['description']
+        if 'category' in data:
+            del data['category']
+        if bugno in remote_db['bugs']:
+            client.delete_bug_info(bugno)
+        for names, file_regex in data.items():
+            for count, entry in enumerate(file_regex):
+                first = True
+                for file_, regexes in entry.items():
+                    boolean = 'and'
+                    if count > 0 and first:
+                        boolean = 'or'
+                    if isinstance(regexes['regexp'], str):
+                        regexes['regexp'] = [regexes['regexp']]
+                    for reg in regexes['regexp']:
+                        if reg:
+                            print(bugno, names, file_, reg, boolean)
+                            client.add_bug_info(
+                                bugno,
+                                names,
+                                file_,
+                                reg,
+                                boolean
+                            )
+                            first = False
+                            boolean = 'and'
 
-#print client.get_bug_info()
+    return client.get_bug_info(force_refresh=True)
 
+
+def main():
+    update_bugs_database(
+        DEFAULT_DB_LOCATION,
+        DEFAULT_COOKIE_LOCATION,
+        DEFAULT_ENDPOINT
+    )
+
+
+if __name__ == '__main__':
+    main()
