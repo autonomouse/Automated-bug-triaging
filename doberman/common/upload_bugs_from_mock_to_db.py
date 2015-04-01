ae83d831d590fe7a9066addb2318860790818df5
Greg Lutostanski <gregory.lutostanski@canonical.com>
2015-04-01 11:12:41 -0500

    add cli frontend

--- doberman/common/upload_bugs_from_mock_to_db.py
+++ doberman/common/upload_bugs_from_mock_to_db.py
@@ -1,15 +1,29 @@
 #!/usr/bin/python
 
 from test_catalog.client.api import TCClient as tc_client
-import yaml
+import argparse
 import json
+import yaml
 
-DEFAULT_DB_LOCATION = '/usr/share/doberman/samples/mock_database.yml'
-DEFAULT_COOKIE_LOCATION = '/etc/oil-ci/test-catalog-auth.json'
+DEFAULT_DB = '/usr/share/doberman/samples/mock_database.yml'
+DEFAULT_TOKEN = '/etc/oil-ci/test-catalog-auth.json'
 DEFAULT_ENDPOINT = 'https://oil.canonical.com/api/'
 
 
-def update_bugs_database(db_location, cookie_location, endpoint):
+def parse():
+    parser = argparse.ArgumentParser(
+        description='Upload bugs to remote database.'
+    )
+    parser.add_argument('-e', '--endpoint', nargs=1, default=DEFAULT_ENDPOINT,
+                        help='Remote test-catalog URL for database interface.')
+    parser.add_argument('-t', '--token', nargs=1, default=DEFAULT_TOKEN,
+                        help='Token/cookie file for test-catalog.')
+    parser.add_argument('-d', '--database', nargs=1, default=DEFAULT_DB,
+                        help='Location of yaml to upload to the database.')
+    return parser.parse_args()
+
+
+def update_bugs_database(endpoint, cookie_location, db_location):
     local_db = yaml.load(open(db_location))
     cookie = json.load(open(cookie_location))
     client = tc_client(endpoint=endpoint, cookies=cookie)
@@ -53,11 +67,8 @@ def update_bugs_database(db_location, cookie_location, endpoint):
 
 
 def main():
-    update_bugs_database(
-        DEFAULT_DB_LOCATION,
-        DEFAULT_COOKIE_LOCATION,
-        DEFAULT_ENDPOINT
-    )
+    args = parse()
+    update_bugs_database(args.endpoint, args.token, args.database)
 
 
 if __name__ == '__main__':
