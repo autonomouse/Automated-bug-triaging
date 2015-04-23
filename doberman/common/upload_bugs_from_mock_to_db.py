5192b6cb09d23bddba33d0610490514a9c8aa3f0
Greg Lutostanski <gregory.lutostanski@canonical.com>
2015-04-23 10:02:19 -0500

    remove argparse nargs and list->element smashing

--- doberman/common/upload_bugs_from_mock_to_db.py
+++ doberman/common/upload_bugs_from_mock_to_db.py
@@ -14,11 +14,11 @@ def parse():
     parser = argparse.ArgumentParser(
         description='Upload bugs to remote database.'
     )
-    parser.add_argument('-e', '--endpoint', nargs=1, default=DEFAULT_ENDPOINT,
+    parser.add_argument('-e', '--endpoint', default=DEFAULT_ENDPOINT,
                         help='Remote test-catalog URL for database interface.')
-    parser.add_argument('-t', '--token', nargs=1, default=DEFAULT_TOKEN,
+    parser.add_argument('-t', '--token', default=DEFAULT_TOKEN,
                         help='Token/cookie file for test-catalog.')
-    parser.add_argument('-d', '--database', nargs=1, default=DEFAULT_DB,
+    parser.add_argument('-d', '--database', default=DEFAULT_DB,
                         help='Location of yaml to upload to the database.')
     parser.add_argument('-w', '--wipe', default=False, action='store_true',
                         help='Wipe the remote database completely.')
@@ -32,9 +32,6 @@ def wipe_bugs_database(client):
 
 
 def update_bugs_database(client, db_location):
-    # not sure of purpose of the following list->first element -- when happens?
-    db_location = db_location[0] if type(db_location) == list else db_location
-
     local_db = yaml.load(open(db_location))
     remote_db = client.get_bug_info(force_refresh=True)
 
@@ -73,16 +70,9 @@ def update_bugs_database(client, db_location):
 
 def main():
     args = parse()
-    # not sure of purpose of getting first element from list (is there a case?)
-    endpoint = args.endpoint
-    if type(endpoint) == list:
-        endpoint = endpoint[0]
-    cookie_location = args.token
-    if type(cookie_location) == list:
-        cookie_location = cookie_location[0]
 
-    cookie = json.load(open(cookie_location))
-    client = tc_client(endpoint=endpoint, cookies=cookie)
+    cookie = json.load(open(args.token))
+    client = tc_client(endpoint=args.endpoint, cookies=cookie)
     if args.wipe:
         wipe_bugs_database(client)
     update_bugs_database(client, args.database)
