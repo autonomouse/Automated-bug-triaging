dcfd09fb0e1d4a378923076083ab2214449e5e72
Darren Hoyland <darren.hoyland@canonical.com>
2015-08-27 18:08:48 +0100

    Temporarily added weebl client back in

---
+++ weebl_python2/test_weebl_python2_client.py
@@ -0,0 +1,10 @@
+import testtools
+
+reportdir = common_test_methods.mock_output_data
+
+
+class WeeblClientTests(testtools.TestCase):
+
+    def test_build_number_in_output(self):
+        # TODO
+        self.assertTrue(False) 
