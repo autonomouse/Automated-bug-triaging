210eb8868e6aa96c7285f62872258a6ccf13bdfc
Darren Hoyland <darren.hoyland@canonical.com>
2016-08-26 12:04:31 +0100

    fixes for doberman

--- doberman/analysis/oil_spill.py
+++ doberman/analysis/oil_spill.py
@@ -233,6 +233,7 @@ class OilSpill(DobermanBase):
                           if "_console.txt" not in k]
                     original_hit_dict = dict(d1 + d2)
 
+                    [value.pop('uuids') for key, value in and_dict.items()]
                     if and_dict == original_hit_dict:
                         links = []
                         url = self.cli.external_jenkins_url
